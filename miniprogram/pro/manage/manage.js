64f24431fe415ecb7459747209e15a0eac101aae
Zerojc031 <champion952@outlook.com>
2019-11-21 23:11:42 +0800

    高级管理员管理页面逻辑
    已架构完成

--- miniprogram/pro/manage/manage.js
+++ miniprogram/pro/manage/manage.js
@@ -22,12 +22,6 @@ Page({
    */
   onReady: function() {
     var that = this
-    db.collection('control').doc('university').get({
-      success: function(res) {
-        that.data.control = res.data
-        console.log('查询control', res.data)
-      }
-    })
     db.collection('university').limit(20).skip(0).get({
       success: function(res) {
         console.log('查询数据库成功1', res)
@@ -131,9 +125,32 @@ Page({
     wx.showLoading({
       title: '更新中',
       success: function() {
-        console.log('control:', that.data.control)
         console.log('arr:', that.data.arr)
-        that.data.control.uniList = that.data.arr
+        db.collection('control').doc('university').get({
+          success: function(res) {
+            that.data.control = res.data
+            console.log('查询control', res.data)
+            // that.data.control.central=res.data.central
+            // that.data.control.east=res.data.east
+            // that.data.control.south=res.data.south
+            // that.data.control.north=res.data.north
+            // that.data.control.northeast=res.data.northeast
+            // that.data.control.northwest=res.data.northwest
+            // that.data.control.southwest=res.data.southwest
+            // that.data.control.others=res.data.others
+            // that.data.control.uniList = that.data.arr
+            for (let i = 0; i < that.data.length; i++) {
+              that.data.control.uniList[i] = that.data.arr[i]
+              that.data.control.uniList[i].isOnShow = res.data.uniList[i].isOnShow
+              if (i == that.data.length - 1) {
+                that.setData({
+                  control: that.data.control,
+                })
+                console.log('更新control完成', that.data.control)
+              }
+            }
+          }
+        })
       },
       complete: function() {
         wx.hideLoading()
@@ -145,25 +162,207 @@ Page({
     })
   },
   show: function(e) {
-    console.log('发布', e)
+    console.log('发布', e.target.id)
+    this.data.control.uniList[e.target.id].isOnShow = true
+    if (this.data.control.uniList[e.target.id].tag == 'south') {
+      for (let i = 0; i < this.data.control.south.length; i++) {
+        if (this.data.control.south[i] == e.target.id) {
+          console.log('已发布过', e.target.id)
+          this.setData({
+            control: this.data.control
+          })
+          this.upLoad()
+          break;
+        }
+        if (i == this.data.control.south.length - 1) {
+          this.data.control.south[i + 1] = e.target.id
+          this.setData({
+            control: this.data.control
+          })
+          this.upLoad()
+          console.log('未发布过', e.target.id)
+          break;
+        }
+      }
+    } else if (this.data.control.uniList[e.target.id].tag == 'central') {
+      for (let i = 0; i < this.data.control.central.length; i++) {
+        if (this.data.control.central[i] == e.target.id) {
+          console.log('已发布过', e.target.id)
+          this.setData({
+            control: this.data.control
+          })
+          this.upLoad()
+          break;
+        }
+        if (i == this.data.control.central.length - 1) {
+          this.data.control.central[i + 1] = e.target.id
+          this.setData({
+            control: this.data.control
+          })
+          this.upLoad()
+          console.log('未发布过', e.target.id)
+          break;
+        }
+      }
+    } else if (this.data.control.uniList[e.target.id].tag == 'north') {
+      for (let i = 0; i < this.data.control.north.length; i++) {
+        if (this.data.control.north[i] == e.target.id) {
+          console.log('已发布过', e.target.id)
+          this.setData({
+            control: this.data.control
+          })
+          this.upLoad()
+          break;
+        }
+        if (i == this.data.control.north.length - 1) {
+          this.data.control.north[i + 1] = e.target.id
+          this.setData({
+            control: this.data.control
+          })
+          this.upLoad()
+          console.log('未发布过', e.target.id)
+          break;
+        }
+      }
+    } else if (this.data.control.uniList[e.target.id].tag == 'east') {
+      for (let i = 0; i < this.data.control.east.length; i++) {
+        if (this.data.control.east[i] == e.target.id) {
+          console.log('已发布过', e.target.id)
+          this.setData({
+            control: this.data.control
+          })
+          this.upLoad()
+          break;
+        }
+        if (i == this.data.control.east.length - 1) {
+          this.data.control.east[i + 1] = e.target.id
+          this.setData({
+            control: this.data.control
+          })
+          this.upLoad()
+          console.log('未发布过', e.target.id)
+          break;
+        }
+      }
+    } else if (this.data.control.uniList[e.target.id].tag == 'northeast') {
+      for (let i = 0; i < this.data.control.northeast.length; i++) {
+        if (this.data.control.northeast[i] == e.target.id) {
+          console.log('已发布过', e.target.id)
+          this.setData({
+            control: this.data.control
+          })
+          this.upLoad()
+          break;
+        }
+        if (i == this.data.control.northeast.length - 1) {
+          this.data.control.northeast[i + 1] = e.target.id
+          this.setData({
+            control: this.data.control
+          })
+          this.upLoad()
+          console.log('未发布过', e.target.id)
+          break;
+        }
+      }
+    } else if (this.data.control.uniList[e.target.id].tag == 'northwest') {
+      for (let i = 0; i < this.data.control.northwest.length; i++) {
+        if (this.data.control.northwest[i] == e.target.id) {
+          console.log('已发布过', e.target.id)
+          this.setData({
+            control: this.data.control
+          })
+          this.upLoad()
+          break;
+        }
+        if (i == this.data.control.northwest.length - 1) {
+          this.data.control.northwest[i + 1] = e.target.id
+          this.setData({
+            control: this.data.control
+          })
+          this.upLoad()
+          console.log('未发布过', e.target.id)
+          break;
+        }
+      }
+    } else if (this.data.control.uniList[e.target.id].tag == 'southwest') {
+      for (let i = 0; i < this.data.control.southwest.length; i++) {
+        if (this.data.control.southwest[i] == e.target.id) {
+          console.log('已发布过', e.target.id)
+          this.setData({
+            control: this.data.control
+          })
+          this.upLoad()
+          break;
+        }
+        if (i == this.data.control.southwest.length - 1) {
+          this.data.control.southwest[i + 1] = e.target.id
+          this.setData({
+            control: this.data.control
+          })
+          this.upLoad()
+          console.log('未发布过', e.target.id)
+          break;
+        }
+      }
+    } else if (this.data.control.uniList[e.target.id].tag == 'others') {
+      for (let i = 0; i < this.data.control.others.length; i++) {
+        if (this.data.control.others[i] == e.target.id) {
+          console.log('已发布过', e.target.id)
+          this.setData({
+            control: this.data.control
+          })
+          this.upLoad()
+          break;
+        }
+        if (i == this.data.control.others.length - 1) {
+          this.data.control.others[i + 1] = e.target.id
+          this.setData({
+            control: this.data.control
+          })
+          this.upLoad()
+          console.log('未发布过', e.target.id)
+          break;
+        }
+      }
+    }
   },
   hide: function(e) {
-    console.log('撤下', e)
+    console.log('撤下', e.target.id)
+    this.data.control.uniList[e.target.id].isOnShow = false
+    this.setData({
+      control: this.data.control
+    })
+    this.upLoad()
   },
-  showID: function(e) {
-    console.log('显示微信号隐藏二维码', e)
-  },
-  hideID: function(e) {
-    console.log('隐藏微信号显示二维码', e)
-  },
-  manage:function(e){
-    console.log('长按管理',e)
-    let temp_id=e.currrentTarget.id
-    // wx.showModal({
-    //   title: '操作',
-    //   content: '',
-    // })
-    //模拟对话框的右边 发布/撤下，左边 显示/隐藏 二维码/微信号
+  upLoad: function() {
+    var that = this
+    db.collection('control').doc('university').update({
+      data: {
+        central: that.data.control.central,
+        east: that.data.control.east,
+        north: that.data.control.north,
+        northeast: that.data.control.northeast,
+        northwest: that.data.control.northwest,
+        others: that.data.control.others,
+        south: that.data.control.south,
+        southwest: that.data.control.southwest,
+        uniList: that.data.control.uniList,
+      },
+      success: function() {
+        wx.showToast({
+          title: '成功',
+          duration: 500,
+        })
+      },
+      fail: function(res) {
+        wx.showToast({
+          title: '失败',
+          duration: 500,
+          icon: 'none'
+        })
+        console.log('上传失败', res)
+      }
+    })
   },
 
   /**
